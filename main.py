9c484268b6169761daf3928b1c5678a0bdd70f9c
Asyraf <trytobepro00@gmail.com>
2022-03-18 16:26:32 +0800

    Added comments

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#import opencv
 import cv2
 
+#declare cap to camera under inheritance of opencv
 cap = cv2.VideoCapture(0)
 
+#while running
 while True:
+    #read data from camera
     success, img = cap.read()
 
+    #show data from camera
     cv2.imshow("Image", img)
+    #dont close
     cv2.waitKey(1)
 
